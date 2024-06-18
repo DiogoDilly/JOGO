65e271c3390b7ed7bd64adeee5590e631af430f5
DiogoDilly <diogodilly.008@gmail.com>
2024-06-18 17:49:30 -0300

    att2

--- jogo.js
+++ jogo.js
@@ -1,19 +1,39 @@
-var tentativas = 0; 
-var advinhacao = Math.floor(Math.random() * 10) +1;
+var tentativas = 0;
+var advinhacao = Math.floor(Math.random() * 10);
+var nombre = document.getElementById("nome");
+document.getElementById("tenta")
+document.getElementById("palavras");
+document.getElementById("palavras2");
+document.getElementById("palavras3")
 function jogo() {
-    
-    document.getElementById("palavras");
     var valordado = document.getElementById("inserido").value;
     if (valordado == advinhacao) {
-       
-        palavras.textContent = " O NUMERO ERA:"+advinhacao+" SUAS TENTATIVAS FORAM:" +tentativas;
-        tentativas = 0;
-        var advinhacao = Math.floor(Math.random() * 10) +1;
-    } else {
-        tentativas = tentativas + 1;       
-        palavras.textContent = "ERROU" ;
+        window.location.href = "fim.html"
+    } else if (valordado > advinhacao) {
+        tentativas = tentativas + 1;
+        palavras.textContent = "ERROU,tente algo menor do que " + valordado;
+        palavras.style.color = "red";
+        tenta.textContent = "tentativas:"+ tentativas;
+    } else if (valordado < advinhacao) {
+        tentativas = tentativas + 1;
+        palavras.textContent = "ERROU,tente algo maior do que " + valordado;
+        palavras.style.color = "red";
+        tenta.textContent = "tentativas:"+ tentativas;
+    }
+    if (tentativas > 0) {
+        tenta.style.color = "green";
+    }
+    if (tentativas > 5) {
+        tenta.style.color = "yellow"
+    }
+    if (tentativas > 15){
+    tenta.style.color = "red"
     }
 }
-function novamente(){
-    Window.location.href = "fim.html";
+function acessar() {
+    window.location.href = "jogo.html"
+}
+function ver() {
+    palavras2.textContent = nombre + " parabens o numero era: " + advinhacao ;
+    palavras2.style.color = "green";
 }
